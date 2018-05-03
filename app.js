f69a6d0794db3c62ea8c291832a3d7a293f6a46e
carotene512 <karoten512@gmail.com>
2018-05-03 23:54:41 +0900

    公開するオブジェクトを限定した。

--- app.js
+++ app.js
@@ -1,74 +1,83 @@
-/** 画像を圧縮するライブラリ */
+(function(global) {
+  var imageResizer = function() {
 
-// Image オブジェクトに src を指定すると、元画像の width と height が取れる
-const img = new Image();
+    /** 画像を圧縮するライブラリ */
 
-/** リーダから読み込んだ値をオブジェクト化 */
-// img.src = fr.result;
-img.crossOrigin = 'anonymous';
-img.src = 'http://secrettalk.me/images/talk/8-2-d2FzYW8uanBn';
+    // Image オブジェクトに src を指定すると、元画像の width と height が取れる
+    const img = new Image();
 
-/** img.src読み込み後の処理 */
-img.onload = function() {
+    /** リーダから読み込んだ値をオブジェクト化 */
+    // img.src = fr.result;
+    img.crossOrigin = 'anonymous';
+    img.src = 'http://secrettalk.me/images/talk/8-2-d2FzYW8uanBn';
 
-  /** 画像をリサイズ */
-  const base64 = self.resizeImage(img);
+    /** img.src読み込み後の処理 */
+    img.onload = function() {
 
-  /** base64形式の画像をバイナリに(バイナリじゃないとサーバに送れないので) */
-  const blob = self.base64ToBlob(base64);
-  console.log(blob);
+      /** 画像をリサイズ */
+      const base64 = self.resizeImage(img);
 
-  //  /** フォームデータの生成 */
-  //  const formData = new FormData();
-  //  formData.append('file', image.getLocalImageData(), image.getImageName());
-};
+      /** base64形式の画像をバイナリに(バイナリじゃないとサーバに送れないので) */
+      const blob = self.base64ToBlob(base64);
+      console.log(blob);
 
-
-var resizeImage =  function(img) {
-
-  /** 画像の読み込み */
-  const width = img.width;
-  const height = img.height;
-
-  // 縮小後のサイズを計算。ここでは横幅 (width) を指定
-  const dstWidth = 1280;
-  const scale = dstWidth / width;
-  const dstHeight = height * scale;
-
-  // Canvas オブジェクトを使い、縮小後の画像を描画
-  const canvas = document.createElement('canvas');
-  canvas.width = dstWidth;
-  canvas.height = dstHeight;
-  const ctx = canvas.getContext('2d');
-  ctx.drawImage(img, 0, 0, dstWidth, dstHeight);
-
-  // Canvas オブジェクトから Data URL を取得
-  const resized = canvas.toDataURL('image/jpeg');
-
-  return resized;
-}
+      //  /** フォームデータの生成 */
+      //  const formData = new FormData();
+      //  formData.append('file', image.getLocalImageData(), image.getImageName());
+    };
 
 
-var base64ToBlob = function(base64) {
+    var resizeImage =  function(img) {
 
-  const base64Data = base64.split(',')[1]; // Data URLからBase64のデータ部分のみを取得
-  const data = window.atob(base64Data); // base64形式の文字列をデコード
-  const buff = new ArrayBuffer(data.length);
-  const arr = new Uint8Array(buff);
+      /** 画像の読み込み */
+      const width = img.width;
+      const height = img.height;
 
-  let blob;
-  let i;
-  let dataLen;
+      // 縮小後のサイズを計算。ここでは横幅 (width) を指定
+      const dstWidth = 1280;
+      const scale = dstWidth / width;
+      const dstHeight = height * scale;
 
-  // blobの生成
-  for (i = 0, dataLen = data.length; i < dataLen; i++) {
-    arr[i] = data.charCodeAt(i);
+      // Canvas オブジェクトを使い、縮小後の画像を描画
+      const canvas = document.createElement('canvas');
+      canvas.width = dstWidth;
+      canvas.height = dstHeight;
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0, dstWidth, dstHeight);
+
+      // Canvas オブジェクトから Data URL を取得
+      const resized = canvas.toDataURL('image/jpeg');
+
+      return resized;
+    }
+
+
+    var base64ToBlob = function(base64) {
+
+      const base64Data = base64.split(',')[1]; // Data URLからBase64のデータ部分のみを取得
+      const data = window.atob(base64Data); // base64形式の文字列をデコード
+      const buff = new ArrayBuffer(data.length);
+      const arr = new Uint8Array(buff);
+
+      let blob;
+      let i;
+      let dataLen;
+
+      // blobの生成
+      for (i = 0, dataLen = data.length; i < dataLen; i++) {
+        arr[i] = data.charCodeAt(i);
+      }
+
+      /** ファイルオブジェクトの生成 */
+      blob = new Blob([arr], {
+        type: 'image/png'
+      });
+
+      return blob;
+    }
+
   }
 
-  /** ファイルオブジェクトの生成 */
-  blob = new Blob([arr], {
-    type: 'image/png'
-  });
+  global.imageResizer = imageResizer;
 
-  return blob;
-}
+})(this);
